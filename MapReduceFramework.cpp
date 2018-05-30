139ce775dcd381fc4eee8da3fa48237de0583d74
sapirely <sapir.elyovitch@mail.huji.ac.il>
2018-05-30 16:04:28 +0300

    Initialization

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file MapReduceFramework.cpp
+ *
+ */
+
+// ------------------------------ includes ------------------------------
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include "MapReduceFramework.h"
+
+
+// ------------------------------- globals ------------------------------
+
+// -------------------------- inner funcs ------------------------------
+
+// declarations so we can keep up with our funcs
+
+
+// ---------------------------- helper methods --------------------------------
+
+// ---------------------------- library methods --------------------------------
+

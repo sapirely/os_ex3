d3e4edec5812113c2486b923ca2d5a9c09725c96
sapirely <sapir.elyovitch@mail.huji.ac.il>
2018-05-30 19:34:08 +0300

    Started writing shuffle

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 #include "MapReduceFramework.h"
 
-
+using namespace std;
 // ------------------------------- globals -----------------------------
 
 // -------------------------- inner funcs ------------------------------
@@ -19,7 +19,58 @@
 
 // ---------------------------- helper methods --------------------------
 
+// primary version
+void shuffle(IntermediateVec intermediateVec){
+
+    // gets sorted intermediary vectors
+    // elements are popped from the back of each vector
+    //todo:
+    // use semaphore for counting vectors
+    // whenever a new vector is inserted to the queue, call sem_post()
+    // use mutex to protect access to the queue
+
+    K2* currentKey = nullptr;
+    // vector of pairs: key, and a vector of values
+    std::vector<std::pair<K2*, std::vector<V2*>>> vecQueue;
+    // maybe shouldn't be declared here - how will it work with the threads?
+    // todo
+
+    // elements are popped from the back of each vector
+    while (!intermediateVec.empty())
+    {
+        // iterate over intermediate vec - insert all values with a certain key
+        // in a sequence
+        if (currentKey == nullptr) // first key
+        {
+            currentKey = intermediateVec.back().first;
+            // create new sequence
+            pair<K2*, std::vector<V2*>> newSequence;
+            vecQueue.push_back(newSequence);
+        }
+        else if (currentKey != intermediateVec.back().first)
+        {
+                // prev key is done -> should change keys
+                currentKey = intermediateVec.back().first;
+                pair<K2*, std::vector<V2*>> newSequence;
+                vecQueue.push_back(newSequence);
+        }
+        // now key is identical to the one at the back of the vector
+
+        // enter value to the sequence (the vector of key CurrentKey)
+        vecQueue.back().second.push_back(intermediateVec.back().second);
+        intermediateVec.pop_back();
+
+    }
+
+
+    // todo - from instructions:
+    // difficult to split efficiently into parallel threads - so we
+    // run parallel with Reduce
+}
+
 // ---------------------------- library methods -------------------------
+
+
 
 /**
  * Called in Map phase
@@ -45,5 +96,7 @@ void emit3 (K3* key, V3* value, void* context){
 void runMapReduceFramework(const MapReduceClient& client,
                            const InputVec& inputVec, OutputVec& outputVec,
                            int multiThreadLevel){
+
+
     return;
 }

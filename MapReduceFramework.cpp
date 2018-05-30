b0bbaa24bef389f992a32f3421f80cd62d2a12dd
sapirely <sapir.elyovitch@mail.huji.ac.il>
2018-05-30 16:10:46 +0300

    Organized main file

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -3,23 +3,47 @@
  *
  */
 
-// ------------------------------ includes ------------------------------
+// ------------------------------ includes ----------------------------
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cassert>
 #include "MapReduceFramework.h"
 
 
-// ------------------------------- globals ------------------------------
+// ------------------------------- globals -----------------------------
 
 // -------------------------- inner funcs ------------------------------
 
 // declarations so we can keep up with our funcs
 
 
-// ---------------------------- helper methods --------------------------------
+// ---------------------------- helper methods --------------------------
 
-// ---------------------------- library methods --------------------------------
+// ---------------------------- library methods -------------------------
 
+/**
+ * Called in Map phase
+ */
+void emit2 (K2* key, V2* value, void* context){
+    return;
+}
+
+/**
+ * Called in Reduce phase
+ */
+void emit3 (K3* key, V3* value, void* context){
+    return;
+}
+
+/**
+ * Runs the MapReduce algorithm.
+ * @param client
+ * @param inputVec - vector of (k1,v1) pairs
+ * @param outputVec - vector of (k3,v3) pairs
+ * @param multiThreadLevel - number of threads to be created
+ */
+void runMapReduceFramework(const MapReduceClient& client,
+                           const InputVec& inputVec, OutputVec& outputVec,
+                           int multiThreadLevel){
+    return;
+}

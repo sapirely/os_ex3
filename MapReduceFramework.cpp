e511aca65bca992ac0251e0fc6bd0c68f1969b67
Naama Kessler <naamak10@gmail.com>
2018-05-31 01:56:23 +0300

    stopped while dealing with: a. where to create intermediateVec? which context should map get every time it is called (and where does it save its output)? b. semaphore-?

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -8,19 +8,93 @@
 #include <iostream>
 #include <vector>
 #include "MapReduceFramework.h"
+#include <pthread.h>
+#include <atomic>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
 // ------------------------------- globals -----------------------------
 
-// -------------------------- inner funcs ------------------------------
+// ---------------------------- Barrier class --------------------------
 
+class Barrier {
+public:
+    Barrier(int numThreads);
+    ~Barrier();
+    void barrier();
+
+private:
+    pthread_mutex_t mutex;
+    pthread_cond_t cv;
+    int count;
+    int numThreads;
+};
+
+Barrier::Barrier(int numThreads) : mutex(PTHREAD_MUTEX_INITIALIZER),
+                                   cv(PTHREAD_COND_INITIALIZER),
+                                   count(0),
+                                   numThreads(numThreads){}
+
+Barrier::~Barrier()
+{
+    if (pthread_mutex_destroy(&mutex) != 0) {
+        fprintf(stderr, "[[Barrier]] error on pthread_mutex_destroy");
+        exit(1);
+    }
+    if (pthread_cond_destroy(&cv) != 0){
+        fprintf(stderr, "[[Barrier]] error on pthread_cond_destroy");
+        exit(1);
+    }
+}
+
+void Barrier::barrier()
+{
+    if (pthread_mutex_lock(&mutex) != 0){
+        fprintf(stderr, "[[Barrier]] error on pthread_mutex_lock");
+        exit(1);
+    }
+    if (++count < numThreads) {
+        if (pthread_cond_wait(&cv, &mutex) != 0){
+            fprintf(stderr, "[[Barrier]] error on pthread_cond_wait");
+            exit(1);
+        }
+    } else {
+        count = 0;
+        if (pthread_cond_broadcast(&cv) != 0) {
+            fprintf(stderr, "[[Barrier]] error on pthread_cond_broadcast");
+            exit(1);
+        }
+    }
+    if (pthread_mutex_unlock(&mutex) != 0) {
+        fprintf(stderr, "[[Barrier]] error on pthread_mutex_unlock");
+        exit(1);
+    }
+}
+
+// -------------------------- helper struct ------------------------------
+/**
+ * Gathers all the variables needed for threadsPart(), since we can pass only 1 arg (void*) in
+ * pthread_create.
+ */
+struct ThreadContext {
+    std::atomic<int>* atomic_counter;
+    const InputVec* inputVec;
+    const MapReduceClient* client;
+    std::vector<IntermediatePair>* intermidiateVec;
+    const int multiThreadLevel;
+    Barrier* barrier;
+};
+
+// -------------------------- inner funcs ------------------------------
 // declarations so we can keep up with our funcs
 
+void* threadsPart(void* arg);
 
-// ---------------------------- helper methods --------------------------
+// ---------------------------- helper methods & structs --------------------------
 
 // primary version
-void shuffle(IntermediateVec intermediateVec){
+void shuffle(IntermediateVec intermediateVec){ // todo: I think that's supposed to be a pointer -?
 
     // gets sorted intermediary vectors
     // elements are popped from the back of each vector
@@ -68,23 +142,63 @@ void shuffle(IntermediateVec intermediateVec){
     // run parallel with Reduce
 }
 
+// ------------------------------- todo's -----------------------------
+// make sure all sys calls are ok
+
+
 // ---------------------------- library methods -------------------------
-
-
 
 /**
  * Called in Map phase
  */
 void emit2 (K2* key, V2* value, void* context){
-    return;
+
 }
 
 /**
  * Called in Reduce phase
  */
 void emit3 (K3* key, V3* value, void* context){
-    return;
+
 }
+
+/**
+ * Performs the actual algorithm.
+ */
+void* threadsPart(void* arg)
+{
+    auto threadCtx = (ThreadContext*) arg;
+    int old_value;
+
+    // call map:
+    K1* key;
+    V1* val;
+    while (*threadCtx->atomic_counter <= threadCtx->inputVec->size())
+        //todo: what does it mean, that a thread that has finished a job returns to the pool?
+        // todo: use pthread_cond_wait instead of the while?
+    {
+        old_value = (*(threadCtx->atomic_counter))++;
+        key = (*threadCtx->inputVec)[old_value].first;
+        val = (*threadCtx->inputVec)[old_value].second;
+        (*threadCtx->client).map(key, val, threadCtx->intermidiateVec);
+    }
+
+    // todo: sort intermediate-pairs vector -> depends on intermediateVec
+//    std::sort((*threadCtx->intermidiateVec).begin(), (*threadCtx->intermidiateVec).end()); // todo: catch exceptions.
+
+    if (!pthread_self()) // if it's the main thread (0)
+    {
+        shuffle(*threadCtx->intermidiateVec);
+    }
+
+    threadCtx->barrier->barrier();
+
+    // reduce:
+    // todo: wait on the semaphor
+
+
+}
+
 
 /**
  * Runs the MapReduce algorithm.
@@ -97,6 +211,24 @@ void runMapReduceFramework(const MapReduceClient& client,
                            const InputVec& inputVec, OutputVec& outputVec,
                            int multiThreadLevel){
 
+    // initialize context (containers pointers, etc.) for all threads:
+    pthread_t threads[multiThreadLevel];
+    std::atomic<int> atomic_pairs_counter(0);
+    ThreadContext threadCtx = {&atomic_pairs_counter, &inputVec, &client, nullptr,
+                               multiThreadLevel, new Barrier(multiThreadLevel)}; //todo: intermediateVec!!!
 
-    return;
+
+    // create threads & atomic counter:
+    int ret;
+    for (int i = 0; i < multiThreadLevel; i++)
+    {
+        ret = pthread_create(&threads[i], nullptr, threadsPart, &threadCtx);
+        if (!ret)
+        {
+            // ERROR, do something.
+        }
+    }
+    threadsPart(nullptr);
+
+
 }
